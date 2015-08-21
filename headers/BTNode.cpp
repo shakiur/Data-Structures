f5c72a39f7ae2ae6b819826b4b7d9aa3a9fcf218
Shakiur <shakiur@gmail.com>
2015-08-20 22:47:02 -0400

    Add append function

--- headers/BTNode.cpp
+++ headers/BTNode.cpp
@@ -19,6 +19,37 @@ class BTNode{
 		void setData(int d) { data = d; }
 		void setLeft(BTNode* l) { left = l; }
 		void setRight(BTNode* r) { right = r; }
+
+		// Add new node
+		void add(int d){
+			BTNode * temp = this;
+			BTNode * add_node = new BTNode(d);
+
+			while(temp){
+				if(temp->getData() < d){
+					if(temp->getLeft()){
+						temp = temp->getLeft();
+					}
+					else{
+						temp->setLeft(add_node);
+						return;
+					}
+				}
+				else if(temp->getData() > d){
+					if(temp->getRight()){
+						temp = temp->getRight();
+					}
+					else {
+						temp->setRight(add_node);
+						return;
+					}
+				}
+				else {
+					delete add_node;
+					return;
+				}
+			}
+		}
 		
 };
 
